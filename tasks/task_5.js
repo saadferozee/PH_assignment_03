8936f32fcff56cb403b869e0648cad4dd23a277d
saadferozee <saadferozee@gmail.com>
2025-08-04 00:15:21 +0600

    task 5 completed

--- tasks/task_5.js
+++ tasks/task_5.js
@@ -1,3 +1,7 @@
 /** Problem 05 - ( PH Email Generator )  */
 var student = { name: "jhankar", roll: 1014, department: "cse" };
 //write your code here
+// concatenating values of student object below.
+var getEmailAddress = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(getEmailAddress);
+// Solved
