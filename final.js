0de4e823ca9cd3206bf687431f79ade05e5cb70b
saadferozee <saadferozee@gmail.com>
2025-08-03 22:47:09 +0600

    added task 1 and 2

---
+++ final.js
@@ -0,0 +1,19 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 2060;
+//write your code here
+var getArea = area / 2;
+console.log(getArea);
+//Completed
+
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money >= 25000) {
+    console.log("Laptop")
+} else if (money >= 10000) {
+    console.log("Cycle")
+} else {
+    console.log("Chocolate")
+}
+//completed
